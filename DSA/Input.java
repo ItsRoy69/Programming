3035f9dc2a8a276c00a1c584eed26ac82a52c10a
itsroy69 <jyotirmoyroy649@gmail.com>
2023-07-31 22:33:27 +0530

    added the dsa files
    Signed-off-by: itsroy69 <jyotirmoyroy649@gmail.com>

---
+++ DSA/Input.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Input {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Please enter some input number: ");
+    int rollno = sc.nextInt();
+    System.out.println("Your roll number is " + rollno);
+
+    System.out.print("Please enter some input text: ");
+    String name = sc.next();
+    System.out.println(name);
+
+    System.out.print("Please enter some input float number: ");
+    float marks = sc.nextFloat();
+    System.out.println(marks);
+
+  }
+}
---
+++ DSA/array.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int arr[5];
+
+    arr[0] = 100;
+    arr[1] = 100;
+    arr[2] = 100;
+    arr[3] = 100;
+    arr[4] = 100;
+
+    printf("\t%d\n", arr[0]);
+    printf("\t%d\n", arr[1]);
+    printf("\t%d\n", arr[2]);
+    printf("\t%d\n", arr[3]);
+    printf("\t%d\n", arr[4]);
+}
