f3ea3a258047e9d60b725762e250f21f4b1fce0e
itsroy69 <jyotirmoyroy649@gmail.com>
2023-08-20 03:11:37 +0530

    added sorting
    Signed-off-by: itsroy69 <jyotirmoyroy649@gmail.com>

---
+++ DSA/Sorting/FindDuplicate.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/find-the-duplicate-number/
+public class FindDuplicate {
+    public int findDuplicate(int[] arr) {
+        for (int i = 0; i < arr.length;) {
+
+            if (arr[i] != i + 1) {
+                int correct = arr[i] - 1;
+                if (arr[i] != arr[correct]) {
+                    swap(arr, i , correct);
+                } else {
+                    return arr[i];
+                }
+            } else {
+                i++;
+            }
+        }
+        return -1;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
