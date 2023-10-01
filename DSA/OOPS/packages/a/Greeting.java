39f99a4779d8371a9238b87608166b85a2778e80
itsroy69 <jyotirmoyroy649@gmail.com>
2023-10-02 03:06:10 +0530

    oops
    Signed-off-by: itsroy69 <jyotirmoyroy649@gmail.com>

---
+++ DSA/OOPS/packages/a/Greeting.java
@@ -0,0 +1,9 @@
+package com.roy.packages.a;
+import static com.roy.packages.b.Message.message;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        message();
+    }
+}
---
+++ DSA/OOPS/singleton/NotSubClass.java
@@ -0,0 +1,9 @@
+
+public class NotSubClass{
+
+
+    public static void main(String[] args) {
+        NotSubClass obj = new NotSubClass();
+//        int n = obj.num;
+    }
+}
