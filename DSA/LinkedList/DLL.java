2e74a9924f2cdf91ac5377df26255537137013b2
itsroy69 <jyotirmoyroy649@gmail.com>
2024-01-23 18:01:20 +0530

    committed
    Signed-off-by: itsroy69 <jyotirmoyroy649@gmail.com>

--- DSA/LinkedList/DLL.java
+++ DSA/LinkedList/DLL.java
@@ -1,97 +1,97 @@
-
 public class DLL {
 
-    private Node head;
+  private Node head;
 
-    public void insertFirst(int val) {
-        Node node = new Node(val);
-        node.next = head;
-        node.prev = null;
-        if (head != null) {
-            head.prev = node;
-        }
-        head = node;
+  public void insertFirst(int val) {
+    Node node = new Node(val);
+    node.next = head;
+    node.prev = null;
+    if (head != null) {
+      head.prev = node;
+    }
+    head = node;
+  }
+
+  public void display() {
+    Node node = head;
+    Node last = null;
+    while (node != null) {
+      System.out.print(node.val + " -> ");
+      last = node;
+      node = node.next;
+    }
+    System.out.println("END");
+
+    System.out.println("Print in reverse");
+    while (last != null) {
+      System.out.print(last.val + " -> ");
+      last = last.prev;
+    }
+    System.out.println("START");
+  }
+
+  public void insertLast(int val) {
+    Node node = new Node(val);
+    Node last = head;
+
+    node.next = null;
+
+    if (head == null) {
+      node.prev = null;
+      head = node;
+      return;
     }
 
-    public void display() {
-        Node node = head;
-        Node last = null;
-        while (node != null) {
-            System.out.print(node.val + " -> ");
-            last = node;
-            node = node.next;
-        }
-        System.out.println("END");
-
-        System.out.println("Print in reverse");
-        while (last != null) {
-            System.out.print(last.val + " -> ");
-            last = last.prev;
-        }
-        System.out.println("START");
+    while (last.next != null) {
+      last = last.next;
     }
 
-    public void insertLast(int val) {
-        Node node = new Node(val);
-        Node last = head;
+    last.next = node;
+    node.prev = last;
+  }
 
-        node.next = null;
+  public Node find(int value) {
+    Node node = head;
+    while (node != null) {
+      if (node.val == value) {
+        return node;
+      }
+      node = node.next;
+    }
+    return null;
+  }
 
-        if (head == null) {
-            node.prev = null;
-            head = node;
-            return;
-        }
+  public void insert(int after, int val) {
+    Node p = find(after);
 
-        while (last.next != null) {
-            last = last.next;
-        }
-
-        last.next = node;
-        node.prev = last;
+    if (p == null) {
+      System.out.println("does not exist");
+      return;
     }
 
-    public Node find(int value) {
-        Node node = head;
-        while (node != null) {
-            if (node.val == value) {
-                return node;
-            }
-            node = node.next;
-        }
-        return null;
+    Node node = new Node(val);
+    node.next = p.next;
+    p.next = node;
+    node.prev = p;
+    if (node.next != null) {
+      node.next.prev = node;
+    }
+  }
+
+  private class Node {
+
+    int val;
+    Node next;
+    Node prev;
+
+    public Node(int val) {
+      this.val = val;
     }
 
-    public void insert(int after, int val) {
-        Node p = find(after);
-
-        if (p == null) {
-            System.out.println("does not exist");
-            return;
-        }
-
-        Node node = new Node(val);
-        node.next = p.next;
-        p.next = node;
-        node.prev = p;
-        if (node.next != null) {
-            node.next.prev = node;
-        }
+    public Node(int val, Node next, Node prev) {
+      this.val = val;
+      this.next = next;
+      this.prev = prev;
     }
-
-    private class Node {
-        int val;
-        Node next;
-        Node prev;
-
-        public Node(int val) {
-            this.val = val;
-        }
-
-        public Node(int val, Node next, Node prev) {
-            this.val = val;
-            this.next = next;
-            this.prev = prev;
-        }
-    }
+  }
 }
